6f1a8b0e7a927fcf2a9338ecdf4d960bd7a41361
pmorris50 <patrickmorris532@gmail.com>
2022-12-05 20:12:50 -0700

    removedcomments

--- script.js
+++ script.js
@@ -54,30 +54,18 @@ function setColor(){
 //  console.log(events.value);
   $('.saveBtn').click('on', function(event){
     console.log('event', event);
-    // we know we clicked on event.target -> $(this).parent().childen(1).val();
+    
 
-    var textValue = $(this).parent().children()[1].value; // what we want to save
-    var workingIndex = $(this).attr('data-hour'); // index we are gopnna save it to
+    var textValue = $(this).parent().children()[1].value; 
+    var workingIndex = $(this).attr('data-hour'); 
     console.log(workingIndex)
     console.log(textValue);
-    var currentlyInStorage = JSON.parse(localStorage.getItem('events')) || []// what was previously saved
-    currentlyInStorage[workingIndex] = textValue; // updated previously saved with the new text in specific index
-    localStorage.setItem('events', JSON.stringify(currentlyInStorage)) // new storage set
+    var currentlyInStorage = JSON.parse(localStorage.getItem('events')) || []
+    currentlyInStorage[workingIndex] = textValue; 
+    localStorage.setItem('events', JSON.stringify(currentlyInStorage)) 
   })
 };
-// render locla storange on page load
-// function renderLocalOnLoad() {
-// var currentStorage = JSON.parse(localStorage.getItem('events')) || [];
-// console.log(currentStorage);
-// if(currentStorage != null){
-//   for(i = 0; i< events.length; i++){
-//     events.textContent = currentStorage;
 
-//   }
-
-// }
-  
-// }
 
 function renderLocalOnLoad(){
   var currentStorage =JSON.parse(localStorage.getItem('events'))
@@ -90,20 +78,7 @@ function renderLocalOnLoad(){
 }
 
 
-  // retrieve local storage
-  // parse that so its understood as array
-  // using a for loop of your choice->read one value from the local storage array and place value in corresponding textarea.
 
-
-//renderLocalOnLoad();
-
-
-        //MAY NOT NEED
-// function eventsPersist(){
-//   $('.description').click(function(note){
-//     note.preventDefualt()
-//    })
-//   }
 
 
 $(function () {
@@ -112,27 +87,7 @@ $(function () {
   setColor();
   renderLocalOnLoad();
   
-  // eventsPersist(); function to keep events on webpage when refreshed... IDEA ALERT.. run a LocalStorage.getItem AFTER LocalStorage.setITEM
- // setInterval(1000);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 
